bca0742ade70971f620c9732ef8abef3fa0b480a
Zack Gilbertson <zack.gilbertson@gmail.com>
2020-01-21 21:27:06 -0600

    -added a shader that makes a blue line around the edges of visable objects on the player view

---
+++ Assets/Shaders/AddShaderToCamera.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddShaderToCamera : MonoBehaviour
+{
+    public Material postProcessingMat;
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        Graphics.Blit(source, destination,postProcessingMat);
+    }
+}
