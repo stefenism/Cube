62cb41598d095b2edc5a9156f305cefd95e36834
stefenism <stefenisms@gmail.com>
2020-03-10 21:05:13 -0500

    did some stuff with the movement...not quite where I want it but I think it's better, setup a very basic solution for not falling off the edge. Good for now

--- Assets/Scripts/Managers/PlayerManager.cs
+++ Assets/Scripts/Managers/PlayerManager.cs
@@ -3,35 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerManager : MonoBehaviour {
-
-    public static PlayerManager playerDaddy = null;
     public PlayerControls player;
-
-
 
     private enum PlayerState{
         LIVING,
         DEAD,
         CLIMBING,
-        PHASING
+        PHASING,
+        EDGING
     }
 
     private PlayerState playerState = PlayerState.LIVING;
 
     void Awake(){
-        if(playerDaddy == null){
-            playerDaddy = this;
-        }
-        else if(playerDaddy != this){
-            Destroy(gameObject);
-        }
-
-        DontDestroyOnLoad(gameObject);
     }
 
-    public void setPlayer(PlayerControls player){
-        playerDaddy.player = player;
-    }
+    // public void setPlayer(PlayerControls player){
+    //     playerDaddy.player = player;
+    // }
 
   
 
@@ -39,11 +28,11 @@ public class PlayerManager : MonoBehaviour {
     public void setPlayerDead(){playerState = PlayerState.DEAD;}
     public void setPlayerClimbing(){playerState = PlayerState.CLIMBING;}
     public void setPlayerPhasing(){playerState = PlayerState.PHASING;}
+    public void setPlayerEdging(){playerState = PlayerState.EDGING;} //I intended to name in that
 
     public bool isPlayerLiving(){return playerState == PlayerState.LIVING;}
     public bool isPlayerDead(){return playerState == PlayerState.DEAD;}
     public bool isPlayerClimbing(){return playerState == PlayerState.CLIMBING;}
     public bool isPlayerPhasing(){return playerState == PlayerState.PHASING;}
-
-
+    public bool isPlayerEdging(){return playerState == PlayerState.EDGING;} //hopefully
 }
