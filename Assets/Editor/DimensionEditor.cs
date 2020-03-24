bf7b79428365eb23c2d193a3715a155374f07eea
Zack Gilbertson <zack.gilbertson@gmail.com>
2020-03-23 20:02:05 -0500

    -Dimension Editor is now a functional tool. No longer breaks when running well playing

--- Assets/Editor/DimensionEditor.cs
+++ Assets/Editor/DimensionEditor.cs
@@ -8,9 +8,11 @@ public class DimensionEditor : EditorWindow
     bool dimSelected = false;
     Dimension[] allDimensions;
     Dimension currentDim;
-    List<GameObject> allGameObjects;
+    List<GameObject> allGameObjects = new List<GameObject>();
     List<GameObject> newGameObjects = new List<GameObject>();
-     
+
+    Vector3 lastCameraPos;
+
     string toggleButtonText = "Start Dimension Editor";
     [MenuItem("Custom Tools/Dimension Editor")]
     public static void ShowWindow()
@@ -33,12 +35,18 @@ public class DimensionEditor : EditorWindow
                 //start
 
                 //enable updates 
-                if (EditorApplication.update != OnEditorUpdate)
-                    EditorApplication.update += OnEditorUpdate;
-                EditorApplication.hierarchyChanged += HierarchyChanged;
 
-                allGameObjects.AddRange(UnityEngine.Object.FindObjectsOfType<GameObject>());//get all objects in scene
-                toggleButtonText = "Stop Dimension Editor";
+                    if (EditorApplication.update != OnEditorUpdate)
+                        EditorApplication.update += OnEditorUpdate;
+                    EditorApplication.hierarchyChanged += HierarchyChanged;
+                    EditorApplication.playModeStateChanged += OnPlayModeChanged;
+
+                    allGameObjects.AddRange(FindObjectsOfType<GameObject>());//get all objects in scene
+
+                    toggleButtonText = "Stop Dimension Editor";
+              
+
+
             }
             else
             {
@@ -57,12 +65,15 @@ public class DimensionEditor : EditorWindow
             }
         }
 
+
+
     }
 
-    void StopCleanup()
+    void StopCleanup()//Stops the Script
     {
         EditorApplication.update -= OnEditorUpdate;
         EditorApplication.hierarchyChanged -= HierarchyChanged;
+        EditorApplication.playModeStateChanged -= OnPlayModeChanged;
         toggleButtonText = "Start Dimension Editor";
         ResetEnabled();
         dimSelected = false;
@@ -102,7 +113,12 @@ public class DimensionEditor : EditorWindow
     {
         currentDim = dim;
         dimSelected = true;
-        Debug.Log("found");
+        //Selection.objects = new Object[0];
+        //Selection.activeGameObject = currentDim.gameObject;
+        Selection.activeObject = currentDim.gameObject;
+        //SceneView.FrameLastActiveSceneView();
+        //Selection.activeObject = null;
+        //GetSceneViewCameraLast();
 
         allDimensions = GameObject.FindObjectsOfType<Dimension>();
 
@@ -116,14 +132,20 @@ public class DimensionEditor : EditorWindow
 
 
 
-    void ResetEnabled()
+    void ResetEnabled()//Backs out of Dimension
     {
+        //MoveSceneViewCameraToLast();
         dimSelected = false;
-        foreach (Dimension d in allDimensions)
+        if(allDimensions != null)
         {
-            
-            d.gameObject.SetActive(true);
+            foreach (Dimension d in allDimensions)
+            {
+
+                d.gameObject.SetActive(true);
+
+            }
         }
+
     }
     void HierarchyChanged()
     {
@@ -167,28 +189,52 @@ public class DimensionEditor : EditorWindow
     }
 
 
-    bool dragging = false;
-    Object[] draggedObjects;
+    private void OnDisable()
+    {
+        StopCleanup();
+    }
+
+    void OnPlayModeChanged(PlayModeStateChange state)
+    {
+        if (state == PlayModeStateChange.ExitingEditMode)
+            StopCleanup();
+
+
+    }
+
+    void MoveSceneViewCameraToLast()
+    {
+        
+        SceneView.lastActiveSceneView.pivot = lastCameraPos;
+        SceneView.lastActiveSceneView.Repaint();
+    }
+
+    void GetSceneViewCameraLast()
+    {
+        lastCameraPos = SceneView.lastActiveSceneView.pivot;
+
+    }
+
 
     protected virtual void OnEditorUpdate()
     {
 
-        if (dimSelected)
-        {
-            Debug.Log("asdfasdf");
-            Event e = Event.current;
-            switch (e.type)
-            {
-                case EventType.KeyDown:
-                    {
-                        if (Event.current.keyCode == (KeyCode.Escape))
-                        {
-                            ResetEnabled();
-                        }
-                        break;
-                    }
-            }
-        }
+        //if (dimSelected)
+        //{
+        //    Debug.Log("asdfasdf");
+        //    Event e = Event.current;
+        //    switch (e.type)
+        //    {
+        //        case EventType.KeyDown:
+        //            {
+        //                if (Event.current.keyCode == (KeyCode.Escape))
+        //                {
+        //                    ResetEnabled();
+        //                }
+        //                break;
+        //            }
+        //    }
+        //}
 
     }
 }
